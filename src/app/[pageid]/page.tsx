e71e6c1d8506276029f74559d88abb799cdfe320
[SW7] 김지훈 <lanslot84@gmail.com>
2024-07-07 05:13:53 +0900

    Refactor: code block, tag list 대응 내용 추가

--- src/app/[pageid]/page.tsx
+++ src/app/[pageid]/page.tsx
@@ -7,35 +7,48 @@ export default async function Detail({ params }: { params: { pageid: string } })
   const data = await getData(params.pageid);
 
   const blocks = Object.values(data.block);
-  console.log(blocks[5]?.value.properties.title[1]);
+  // console.log(blocks[5]?.value.properties.title[1]);
+  // console.log(blocks[0].value.properties["N~sy"][0]);
   // console.log(data.block["6b0b5e1c-9ac2-40ba-a2b4-9664cadb2c5c"].value.properties.title);
 
   return (
     <>
-      <span className="capsule-label">{blocks[0].value.properties["N~sy"][0]}</span>
+      <div className="flex gap-2">
+        {blocks[0].value.properties["N~sy"][0][0].split(",").map((tag: string, index: number) => (
+          <span key={`${index}_${tag}`} className="capsule-label">
+            {tag}
+          </span>
+        ))}
+      </div>
+
       <h1 className="text-2xl font-bold mt-3">{blocks[0].value.properties.title}</h1>
       {blocks.length > 3 ? (
         <ul className="py-4 flex flex-col gap-2">
           {blocks
             .filter((block, index) => index >= 3)
             .map((block, index) => {
+              console.log(block.value.properties?.title[0]);
               if (block.value.properties?.title) {
                 return (
-                  <li key={index}>
-                    {block.value.properties.title.map((text: String[], index: number) => {
-                      return (
-                        <span key={index} className={text[1]?.[0][0] === "b" ? "font-bold text-indigo-600" : ""}>
-                          {text[0]}
-                        </span>
-                      );
-                    })}
+                  <li key={index} className={block.value.type === "code" ? "code-block" : ""}>
+                    {block.value.type === "code" ? (
+                      <code>{block.value.properties.title[0][0].replace(/\\n/gi, "</br>")}</code>
+                    ) : (
+                      block.value.properties.title.map((text: String[], index: number) => {
+                        return (
+                          <span key={index} className={text[1]?.[0][0] === "b" ? "font-bold text-indigo-600" : ""}>
+                            {text[0]}
+                          </span>
+                        );
+                      })
+                    )}
                   </li>
                 );
               }
             })}
         </ul>
       ) : (
-        <p className="py-4 text-sm text-gray-500">작성된 내용이 없습니다.</p>
+        <p className="py-5 text-sm text-gray-500">아직 작성된 내용이 없습니다. 조금만 기다려 주세요!</p>
       )}
       {/* <NotionRenderer recordMap={data} fullPage={true} darkMode={false} rootPageId={params.pageid} previewImages /> */}
     </>
