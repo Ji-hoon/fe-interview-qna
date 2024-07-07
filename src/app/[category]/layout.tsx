4cce636b0fff875cb6fd9c4b9504b7d58327409d
[SW7] 김지훈 <lanslot84@gmail.com>
2024-07-07 18:09:07 +0900

    Refactor: header 여백 조정

--- src/app/[category]/layout.tsx
+++ src/app/[category]/layout.tsx
@@ -13,19 +13,18 @@ export default async function CategoryLayout({
   return (
     <>
       <Header>
-        <p className="w-[100px]">
+        <p className="w-[80px]">
           <Link href={"/"} className="flex gap-1">
-            {/* <span className="arrow">←</span> */}
-            메인으로
+            {/* <span className="arrow">←</span> */}홈으로
           </Link>
         </p>
         {params.pageid || (
           <h3 className="grow justify-center flex gap-2 font-bold text-xl">
-            {params.category} 카테고리
+            {params.category}
             <span className="capsule-label">{pages.length}개의 질문</span>
           </h3>
         )}
-        <p className="w-[100px]"></p>
+        <p className="w-[80px]"></p>
       </Header>
       <div className="p-10 pt-8 category-container">{children}</div>
     </>
